e7145cc94965a708446e2ff79b9c1c7baeb08026
Manuel <manuelsilvadois@gmail.com>
2020-01-17 18:45:51 +0000

    Refactored routing to App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeneratorPageComponent } from './pages/generator-page/generator-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'generator', component: GeneratorPageComponent },
+  { path: '', redirectTo: '/generator', pathMatch: 'full' },
+];
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,10 @@ import { GeneratorPageComponent } from './pages/generator-page/generator-page.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
