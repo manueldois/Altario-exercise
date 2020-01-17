5f20c9fc74d2b536d52d34fddbbd5749823f417a
Manuel <manuelsilvadois@gmail.com>
2020-01-17 18:39:42 +0000

    Created generator page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneratorPageComponent } from './pages/generator-page/generator-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneratorPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/generator-page/generator-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './generator-page.component.html',
+  styleUrls: ['./generator-page.component.scss']
+})
+export class GeneratorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
